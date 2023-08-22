2e274f6e418cc3fa9ee64c551f601ec890a77f80
tuyenldhust <tuyen.ld194715@sis.hust.edu.vn>
2023-08-22 15:32:16 +0700

    Django Tutorial Part 6: Generic list and detail views

--- catalog/models.py
+++ catalog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.urls import reverse
 import uuid # Required for unique book instances
+from django.utils.translation import gettext_lazy as _ 
 
 class Genre(models.Model):
   """Model representing a book genre."""
-  name = models.CharField(max_length=200, help_text='Enter a book genre (e.g.Science Fiction)')
+  name = models.CharField(_('Genre'), max_length=200, help_text=_('Enter a book genre (e.g.Science Fiction)'))
 
   def __str__(self):
     """String for representing the Model object."""
@@ -12,11 +13,11 @@ class Genre(models.Model):
 
 class Book(models.Model):
   """Model representing a book (but not a specific copy of a book)."""
-  title = models.CharField(max_length=200)
-  author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-  summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
-  isbn = models.CharField('ISBN', max_length=13, unique=True,help_text='13 Character <ahref="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
-  genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+  title = models.CharField(_('Title'), max_length=200)
+  author = models.ForeignKey('Author', verbose_name=_('Author'), on_delete=models.SET_NULL, null=True)
+  summary = models.TextField(_('Summary'), max_length=1000, help_text=_('Enter a brief description of the book'))
+  isbn = models.CharField('ISBN', max_length=13, unique=True,help_text=_('13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>'))
+  genre = models.ManyToManyField(Genre, verbose_name=_('Genre'), help_text=_('Select a genre for this book'))
   
   def __str__(self):
     """String for representing the Model object."""
@@ -35,12 +36,12 @@ class Book(models.Model):
   
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular bookacross whole library')
-    book = models.ForeignKey('Book', on_delete=models.RESTRICT)
-    imprint = models.CharField(max_length=200)
-    due_back = models.DateField(null=True, blank=True)
-    LOAN_STATUS = (('m', 'Maintenance'),('o', 'On loan'),('a', 'Available'),('r', 'Reserved'),)
-    status = models.CharField(max_length=1,choices=LOAN_STATUS,blank=True,default='m',help_text='Book availability',)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text=_('Unique ID for this particular bookacross whole library'))
+    book = models.ForeignKey('Book', verbose_name=_('Book'), on_delete=models.RESTRICT)
+    imprint = models.CharField(_('Imprint'), max_length=200)
+    due_back = models.DateField(_('Due back'), null=True, blank=True)
+    LOAN_STATUS = (('m', _('Maintenance')),('o', _('On loan')),('a', _('Available')),('r', _('Reserved')),)
+    status = models.CharField(_('Status'), max_length=1,choices=LOAN_STATUS,blank=True,default='m',help_text=_('Book availability'),)
     
     class Meta:
        ordering = ['due_back']
@@ -51,10 +52,10 @@ class BookInstance(models.Model):
     
 class Author(models.Model):
     """Model representing an author."""
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Died', null=True, blank=True)
+    first_name = models.CharField(_('First name'), max_length=100)
+    last_name = models.CharField(_('Last name'), max_length=100)
+    date_of_birth = models.DateField(_('Birthday'), null=True, blank=True)
+    date_of_death = models.DateField(_('Died'), null=True, blank=True)
 
     class Meta:
         ordering = ['last_name', 'first_name']
